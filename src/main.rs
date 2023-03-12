3f9c1541123db1c4ec36d987898f9f3cc8406da8
Charlie Mickey <118931297+charlesmickey222@users.noreply.github.com>
2023-03-11 18:32:26 -0600

    add number input to terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::io; //  std=stnadard library || io=input/output library 
+
+fn main(){
+    println!("Guess the Number ");
+
+    println!("i bet the guess you type is going to be wrong and stupid");
+
+    let mut guess = String::new(); // let mut = mutable 
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("you guessed: {guess}");
 }
