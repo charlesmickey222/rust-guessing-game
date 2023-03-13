731dd8748389044e678da445a63de061e89938d5
Charlie Mickey <118931297+charlesmickey222@users.noreply.github.com>
2023-03-13 09:10:09 -0500

    finish number guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
 use std::io; //  std=stnadard library || io=input/output library 
+use std::cmp::Ordering;
+use rand::Rng;
+
 
 fn main(){
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
     println!("Guess the Number ");
 
     println!("i bet the guess you type is going to be wrong and stupid");
+    println!("{secret_number}");
+     // let mut = mutable 
+    
 
-    let mut guess = String::new(); // let mut = mutable 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    println!("you guessed: {guess}");
+        let guess: u32 =  match guess.trim().parse() {
+            Ok(num)=> num,
+            Err(_) => continue,
+        };
+    
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("to small silly  guess again"),
+        Ordering::Greater => println!("way too big idiot, guess again"),
+        Ordering::Equal =>{ 
+            println!("lucky guess youre still dumb");
+            break;
+        }
+    }}
 }
